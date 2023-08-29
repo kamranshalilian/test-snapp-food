ff58cdd5d15b86c9f19bce8c912d898fafa5ccb9
kamran <shaliliankamran@gmail.com>
2023-08-29 23:37:03 +0330

    create route and make controller and add pdf rule create  DelayController.php  Software Engineer Task.pdf update  api.php

---
+++ app/Http/Controllers/DelayController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DelayController extends Controller
+{
+    public function report()
+    {
+        return 1;
+    }
+
+    public function alert()
+    {
+        return 2;
+
+    }
+
+    public function assign()
+    {
+        return 3;
+    }
+}
