a91943059f7a1a3f5dcddcecd595c04a98f80f2e
Kamran <shaliliankamran@gmail.com>
2023-08-30 10:06:36 +0330

    make DelayController and Request update  DelayController.php create  AlertRequest.php  AssignRequest.php

--- app/Http/Controllers/DelayController.php
+++ app/Http/Controllers/DelayController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\AlertRequest;
+use App\Http\Requests\AssignRequest;
 use Illuminate\Http\Request;
 
 class DelayController extends Controller
@@ -11,13 +13,13 @@ class DelayController extends Controller
         return 1;
     }
 
-    public function alert()
+    public function alert(AlertRequest $request)
     {
         return 2;
 
     }
 
-    public function assign()
+    public function assign(AssignRequest $request)
     {
         return 3;
     }
