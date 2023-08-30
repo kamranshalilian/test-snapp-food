bd2b4a857d96e0142b7a5f8c1145a8093df5afc4
Kamran <shaliliankamran@gmail.com>
2023-08-30 17:23:02 +0330

    make index api and Resource and controller update  api.php create  AgentController.php  AgentResource.php  OrderController.php  OrderResource.php  VendorController.php  VendorResource.php

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Http\Resources\OrderResource;
+use App\Models\Order;
+
+
+class OrderController extends Controller
+{
+    public function index()
+    {
+        return OrderResource::collection(Order::get());
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\DelayController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\VendorController;
+use App\Http\Controllers\AgentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,6 +22,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get("orders", [OrderController::class, "index"]);
+Route::get("agents", [AgentController::class, "index"]);
+Route::get("vendors", [VendorController::class, "index"]);
 Route::get("delay/report", [DelayController::class, "report"]);
 Route::post("delay/alert", [DelayController::class, "alert"]);
 Route::post("delay/assign", [DelayController::class, "assign"]);
